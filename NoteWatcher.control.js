4a50ca733c2593e28863f5d6888ea4559780714d
scrutinizer42 <universejockey@gmail.com>
2025-03-29 22:47:53 +0100

    Add files via upload

--- NoteWatcher.control.js
+++ NoteWatcher.control.js
@@ -7,10 +7,11 @@ host.setShouldFailOnDeprecatedUse(true);
 host.defineController("TheUniverse", "NoteWatcher", "0.3", "9808c2c7-f29d-46e9-8c24-e942e3242832", "universejockey");
 host.defineMidiPorts(1, 1);
 
-const mListScale = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
+const mListScale = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'H'];
 const mMidiOff = 128;
 const mMidiOn = 144;
 
+// the amount of pressed keys:
 var mPlayedNoteCount = 0;
 
 var mPlayedNoteList = [];
@@ -19,7 +20,18 @@ let mPlayedNoteCountLabel;
 let mAllowedMajorScaleLabel;
 let mAllowedMinorScaleLabel;
 
-const mMajorNoteListByScale = 
+// keeps the possible scales for up to 12 played notes for major scales:
+let mMajorPlayedNoteList;
+
+// keeps the possible scales for up to 12 played notes for minor scales:
+let mMinorPlayedNoteList; 
+
+mMajorAllowedScales = [0,0,0,0,0,0,0,0,0,0,0,0];
+
+mMinorAllowedScales = [0,0,0,0,0,0,0,0,0,0,0,0];
+
+
+const mMajorScaleListByNote = 
 [
   // c,c#, d,d#, e, f,f#, g,g#, a,a#, h  
    [ 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1],  // c   major 0#
@@ -36,114 +48,55 @@ const mMajorNoteListByScale =
    [ 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1],  // h   major 5#
 ];
 
-const mMajorPlayed2ScaleMap =
-[
-//  c # d # e f # g # a # h - scales
-   [1,0,0,1,0,1,0,1,1,0,1,0], //c   played
-   [0,1,1,0,1,0,1,0,1,1,0,1], //c#  played
-   [1,0,1,1,0,1,0,1,0,1,1,0], //d   played
-   [0,1,0,1,1,0,1,0,1,0,1,1], //d#  played
-   [1,0,1,0,1,1,0,1,0,1,0,1], //e   played
-   [1,1,0,1,0,1,1,0,1,0,1,0], //f   played
-   [0,1,1,0,1,0,1,1,0,1,0,1], //f#  played
-   [1,0,1,1,0,1,0,1,1,0,1,0], //g   played
-   [0,1,0,1,1,0,1,0,1,1,0,1], //g#  played
-   [1,0,1,0,1,1,0,1,0,1,1,0], //a   played
-   [0,1,0,1,0,1,1,0,1,0,1,1], //a#  played
-   [1,1,1,0,1,0,1,1,0,1,0,1], //h   played
-];
-
-mMajorPlayedNoteList =
-[
-   [0,0,0,0,0,0,0,0,0,0,0,0], //  1. played
-   [0,0,0,0,0,0,0,0,0,0,0,0], //  2. played
-   [0,0,0,0,0,0,0,0,0,0,0,0], //  3. played
-   [0,0,0,0,0,0,0,0,0,0,0,0], //  4. played
-   [0,0,0,0,0,0,0,0,0,0,0,0], //  5. played
-   [0,0,0,0,0,0,0,0,0,0,0,0], //  6. played
-   [0,0,0,0,0,0,0,0,0,0,0,0], //  7. played
-   [0,0,0,0,0,0,0,0,0,0,0,0], //  8. played
-   [0,0,0,0,0,0,0,0,0,0,0,0], //  9. played
-   [0,0,0,0,0,0,0,0,0,0,0,0], // 10. played
-   [0,0,0,0,0,0,0,0,0,0,0,0], // 11. played
-   [0,0,0,0,0,0,0,0,0,0,0,0], // 12. played
-];
-
-
-mMajorAllowedScales = [0,0,0,0,0,0,0,0,0,0,0,0];
-
-
-const mMinorNoteListByScale = 
+// unused
+const mMinorScaleListByNote = 
 [
   // c,c#, d,d#, e, f,f#, g,g#, a,a#, h 
    [ 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0],  // c   minor 3b
    [ 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1],  // c#  minor 4#  
    [ 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0],  // d   minor 1b
    [ 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1],  // dis minor 6#  
-   [ 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1],  // e   minor 1#
+   [ 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1],  // e   minor 1#
    [ 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0],  // f   minor 4b
    [ 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1],  // fis minor 3#
    [ 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0],  // g   minor 2b
    [ 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1],  // gis minor 5#
-   [ 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0],  // a   minor 0#
+   [ 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1],  // a   minor 0#
    [ 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0],  // b   minor 5b
    [ 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1],  // h   minor 2#
-
 ];
 
-const mMinorPlayed2ScaleMap =
-[
-//  c # d # e f # g # a # h - scales
-   [1,0,1,0,1,1,0,1,0,1,1,0], //c   played
-   [0,1,0,1,0,1,1,0,1,0,1,1], //c#  played
-   [1,0,1,0,1,0,1,1,0,1,0,1], //d   played
-   [1,1,0,1,0,1,0,1,1,0,1,0], //d#  played
-   [0,1,1,0,1,0,1,0,1,1,0,1], //e   played
-   [1,0,1,1,0,1,0,1,0,1,1,0], //f   played
-   [0,1,0,1,1,0,1,0,1,0,1,1], //f#  played
-   [1,0,1,0,1,1,0,1,0,1,0,1], //g   played
-   [1,1,0,1,0,1,1,0,1,0,1,0], //g#  played
-   [0,1,1,0,1,0,1,1,0,1,0,1], //a   played
-   [1,0,1,1,1,1,0,1,1,0,1,0], //a#  played
-   [0,1,0,1,1,0,1,0,1,0,0,1], //h   played
-];
 
-mMinorPlayedNoteList =
-[
-   [0,0,0,0,0,0,0,0,0,0,0,0], //  1. played
-   [0,0,0,0,0,0,0,0,0,0,0,0], //  2. played
-   [0,0,0,0,0,0,0,0,0,0,0,0], //  3. played
-   [0,0,0,0,0,0,0,0,0,0,0,0], //  4. played
-   [0,0,0,0,0,0,0,0,0,0,0,0], //  5. played
-   [0,0,0,0,0,0,0,0,0,0,0,0], //  6. played
-   [0,0,0,0,0,0,0,0,0,0,0,0], //  7. played
-   [0,0,0,0,0,0,0,0,0,0,0,0], //  8. played
-   [0,0,0,0,0,0,0,0,0,0,0,0], //  9. played
-   [0,0,0,0,0,0,0,0,0,0,0,0], // 10. played
-   [0,0,0,0,0,0,0,0,0,0,0,0], // 11. played
-   [0,0,0,0,0,0,0,0,0,0,0,0], // 12. played
-];
 
-mMinorAllowedScales = [0,0,0,0,0,0,0,0,0,0,0,0];
 
-function addAllowedScale2PlayList(playedNoteIndex,mapIndex)
+function addAllowedScale2PlayList(playedNoteCount,midiNoteIndex)
 {
+    if(playedNoteCount < 0)
+   {
+      return;
+   }
+
    for(let i = 0;i < 12;i++)
    {
-      mMajorPlayedNoteList[playedNoteIndex][i] = mMajorPlayed2ScaleMap[mapIndex][i];
+      mMajorPlayedNoteList[playedNoteCount][i] = mMajorScaleListByNote[midiNoteIndex][i];
 
-      mMinorPlayedNoteList[playedNoteIndex][i] = mMinorPlayed2ScaleMap[mapIndex][i];
+      mMinorPlayedNoteList[playedNoteCount][i] = mMinorScaleListByNote[midiNoteIndex][i];
    }
-   for(let i = 0; i < 12;i++)
+   for(let i = 0; i < (playedNoteCount+1);i++)
    {
-      ShowArrayContent(mMajorPlayedNoteList[i],"mMajorPlayedNoteList", i);
-
-      ShowArrayContent(mMinorPlayedNoteList[i],"mMinorPlayedNoteList", i);
+      ShowArrayContent(mMajorScaleListByNote[i],"mMajorPlayedNoteList", i);
+   
+      //ShowArrayContent(mMinorScaleListByNote[i],"mMinorPlayedNoteList", i);
    } 
 }
 
 function removeAllowedScaleFromPlayList(playedNoteIndex)
 {
+   if(playedNoteIndex < 0)
+   {
+      return;
+   }
+   
    mMajorPlayedNoteList[playedNoteIndex].fill(0);
 
    mMinorPlayedNoteList[playedNoteIndex].fill(0);
@@ -161,7 +114,7 @@ function calcAllowedScales(mPlayedNoteCount)
 
       mMinorAllowedScales.fill(0);
 
-      println("no scales found");
+      //println("no scales found");
    }
    else
    {
@@ -213,11 +166,11 @@ function calcAllowedScales(mPlayedNoteCount)
 
       }
 
-      println("major scales found: " + allowedMajorScales);
-      println("minor scales found: " + allowedMinorScales);
+      //println("major scales found: " + allowedMajorScales);
+      //println("minor scales found: " + allowedMinorScales);
 
    }
-
+ 
    mAllowedMajorScaleLabel.set(allowedMajorScales);
    mAllowedMinorScaleLabel.set(allowedMinorScales);
    
@@ -248,8 +201,16 @@ function InitUi1Watcher()
 
 function init() {
 
-  initSimpleWatcher();
-  InitUi1Watcher();
+
+
+   mMajorPlayedNoteList = Array.from(Array(12), () => new Array(12));
+   mMinorPlayedNoteList = Array.from(Array(12), () => new Array(12));
+
+
+   initSimpleWatcher();
+   InitUi1Watcher();
+
+
 
    
    println("NoteWatcher initialized!");
@@ -285,22 +246,22 @@ function evaluate(status, data1, data2)
 {
    // show played note and count of pressed keys:
 
-   var idx = data1 % 12;
-   var note = mListScale[idx];
+   var midiNoteIndex = data1 % 12;  // from 0 - 11
+   var note = mListScale[midiNoteIndex];
 
    if(status == mMidiOff)
    {
-      mPlayedNoteList[idx].set("");
+      mPlayedNoteList[midiNoteIndex].set("");
       mPlayedNoteCount--;
       removeAllowedScaleFromPlayList(mPlayedNoteCount);
 
    }
    else if(status == mMidiOn)
    {
-      mPlayedNoteList[idx].set("X");
+      mPlayedNoteList[midiNoteIndex].set("X");
 
       // add allowed scales from a played key:
-      addAllowedScale2PlayList(mPlayedNoteCount,idx);
+      addAllowedScale2PlayList(mPlayedNoteCount,midiNoteIndex);
 
       mPlayedNoteCount++;
    }
@@ -316,3 +277,50 @@ function onMidi0(status, data1, data2)
 
     //printMidi(status, data1, data2);
 }
+
+
+// unused stuff:
+
+// const mMajorPlayed2ScaleMap =
+// [
+// //  c # d # e f # g # a # h - scales
+//    [1,0,0,1,0,1,0,1,1,0,1,0], //c   played
+//    [0,1,1,0,1,0,1,0,1,1,0,1], //c#  played
+//    [1,0,1,1,0,1,0,1,0,1,1,0], //d   played
+//    [0,1,0,1,1,0,1,0,1,0,1,1], //d#  played
+//    [1,0,1,0,1,1,0,1,0,1,0,1], //e   played
+//    [1,1,0,1,0,1,1,0,1,0,1,0], //f   played
+//    [0,1,1,0,1,0,1,1,0,1,0,1], //f#  played
+//    [1,0,1,1,0,1,0,1,1,0,1,0], //g   played
+//    [0,1,0,1,1,0,1,0,1,1,0,1], //g#  played
+//    [1,0,1,0,1,1,0,1,0,1,1,0], //a   played
+//    [0,1,0,1,0,1,1,0,1,0,1,1], //a#  played
+//    [1,1,1,0,1,0,1,1,0,1,0,1], //h   played
+// ];
+
+// const mMinorPlayed2ScaleMap =
+// [
+// //  c # d # e f # g # a # h - scales
+//    [1,0,1,0,1,1,0,1,0,1,1,0], //c   played midi#0
+//    [0,1,0,1,0,1,1,0,1,0,1,1], //c#  played midi#1
+//    [1,0,1,0,1,0,1,1,0,1,0,1], //d   played midi#2
+//    [1,1,0,1,0,1,0,1,1,0,1,0], //d#  played midi#3
+//    [0,1,1,0,1,0,1,0,1,1,0,1], //e   played midi#4
+//    [1,0,1,1,0,1,0,1,0,1,1,0], //f   played midi#5
+//    [0,1,0,1,1,0,1,0,1,0,1,1], //f#  played midi#6
+//    [1,0,1,0,1,1,0,1,0,1,0,1], //g   played midi#7
+//    [1,1,0,1,0,1,1,0,1,0,1,0], //g#  played midi#8
+//    [0,1,1,0,1,0,1,1,0,1,0,1], //a   played midi#9
+//    [1,0,1,1,1,1,0,1,1,0,1,0], //a#  played midi#10
+//    [0,1,0,1,1,0,1,0,1,0,0,1], //h   played midi#11
+// ];
+
+
+
+
+
+
+
+
+
+
